398baaa5cea405366f07c701eb387b1c68953208
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-07-19 14:39:39 +0300

    done

---
+++ BuildingForms/BuildingForms/Controllers/Home.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BuildingForms.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingForms.Controllers
+{
+	public class Home:Controller{
+        [HttpGet]
+        public IActionResult Index()
+		{
+			return View(ProductRepository.Products);
+		}
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            ProductRepository.AddProduct(p);
+            return RedirectToAction(actionName:"Index", controllerName: "Home");
+        }
+        [HttpGet]
+        public IActionResult Search(string q)
+        {
+            if (string.IsNullOrWhiteSpace(q)) {
+                return View();
+            }
+            return View("Index", ProductRepository.Products.Where(i=>i.Name.Contains(q)));
+        }
+    }
+}
+
---
+++ BuildingForms/BuildingForms/Models/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BuildingForms.Models
+{
+	public static class ProductRepository
+	{
+		private static List<Product> _products;
+		public static List<Product> Products {
+			get {
+				return _products;
+			}
+		}
+		static ProductRepository(){
+			_products = new List<Product>() {
+				new Product(){Id = 1, Description = "1Desc", isApproved = false,Name = "1nsame", Price = 9999},
+				new Product(){Id = 2, Description = "2Desc", isApproved = false,Name = "2nsame", Price = 9999},
+				new Product(){Id = 3, Description = "3Desc", isApproved = false,Name = "3nsame", Price = 9999},
+				new Product(){Id = 4, Description = "4Desc", isApproved = false,Name = "4nsame", Price = 9999},
+				new Product(){Id = 5, Description = "5Desc", isApproved = false,Name = "5nsame", Price = 9999}
+			};	
+		}
+		public static void AddProduct(Product product) {
+			_products.Add(product);
+		}
+	}
+}
+
